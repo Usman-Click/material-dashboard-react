7d89c62b621fe53deb61c0acfec224f25231eb73
Usman-Click <47116342+Usman-Click@users.noreply.github.com>
2025-07-14 15:56:44 +0100

    fix: update brand name in Sidenav and improve logo display comments

--- src/examples/Sidenav/index.js
+++ src/examples/Sidenav/index.js
@@ -160,13 +160,15 @@ function Sidenav({ color, brand, brandName, routes, ...rest }) {
             <Icon sx={{ fontWeight: "bold" }}>close</Icon>
           </MDTypography>
         </MDBox>
+        {/* Brand logo */}
         <MDBox component={NavLink} to="/" display="flex" alignItems="center">
-          {brand && <MDBox component="img" src={brand} alt="Brand" width="2rem" />}
+          {/* {brand && <MDBox component="img" src={brand} alt="Brand" width="2rem" />} */}
           <MDBox
             width={!brandName && "100%"}
             sx={(theme) => sidenavLogoLabel(theme, { miniSidenav })}
           >
-            <MDTypography component="h6" variant="button" fontWeight="medium" color={textColor}>
+            {/* Brand name */}
+            <MDTypography component="h6" variant="button" fontWeight="bold" color={textColor}>
               {brandName}
             </MDTypography>
           </MDBox>
